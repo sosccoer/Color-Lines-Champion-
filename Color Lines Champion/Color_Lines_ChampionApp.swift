5fa249aedb22548884aa09d6c6e7a199d88efa22
soscooter <lelya.rumynin@gmail.com>
2024-03-29 19:05:42 +0300

    Initial Commit

---
+++ Color Lines Champion/Color_Lines_ChampionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_Lines_ChampionApp.swift
+//  Color Lines Champion
+//
+//  Created by lelya.rumynin@gmail.com on 29.03.24.
+//
+
+import SwiftUI
+
+@main
+struct Color_Lines_ChampionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
